7bbfe1e48214572fae66de9338e9335c330f4968
EPound <epound@gmail.com>
2020-07-25 14:09:38 -0400

    added tests

--- test/test_inference.py
+++ test/test_inference.py
@@ -1,1 +1,32 @@
-from unittest import TestCase
+import unittest
+
+# import inference
+import handler
+
+
+class TestHandler(unittest.TestCase):
+
+    def test_get_files(self):
+        self.result = handler.get_files('input/post')
+        self.list_pre = ['hurricane-matthew_00000351_post_disaster.png']
+        self.assertEqual(self.list_pre, self.result)
+
+    def test_get_files_mixed_case(self):
+        self.result = handler.get_files('input/pre')
+        self.list_pre = ['hurricane-matthew_00000351_pre_disaster.PNG']
+        self.assertEqual(self.list_pre, self.result)
+
+    def test_string_equal_length(self):
+        st1 = ['hurricane-matthew_00000351_pre_disaster.png']
+        st2 = ['hurricane-matthew_00000351_pre_disaster.PNG']
+        self.assertTrue(handler.file_valid_check(st1, st2))
+
+    def test_string_unequal_length(self):
+        st1 = ['hurricane-matthew_00000351_pre_disaster.PNG',
+               'hurricane-matthew_00000351_post_disaster.png']
+        st2 = ['hurricane-matthew_00000351_pre_disaster.PNG']
+        self.assertFalse(handler.file_valid_check(st1, st2))
+
+    def test_name_mismatch(self):
+        pass
+
