847c81fb1371ce3f7a402fe6063c0d23c02e8983
EPound <epound@gmail.com>
2020-07-28 08:20:34 -0400

    create object class

--- handler.py
+++ handler.py
@@ -2,32 +2,89 @@ import fnmatch
 import os
 import re
 
-PRE_DIR = 'sample_data/input/pre'
-POST_DIR = 'sample_data/input/post'
-OUTPUT_DIR = 'sample_data/output'
+import inference
+
+PRE_DIR = 'test_dir/input/pre'
+POST_DIR = 'test_dir/input/post'
+OUTPUT_DIR = 'test_dir/output'
+
+
+class Files(object):
+
+    def __init__(self, pre, post):
+        self.pre = os.path.abspath(os.path.join(PRE_DIR, pre))
+        self.post = os.path.abspath(os.path.join(POST_DIR, post))
+        self.base_num = self.check_base_num()
+        self.output_loc = os.path.abspath(os.path.join(OUTPUT_DIR, 'loc', f'{self.base_num}_loc.png'))
+        self.output_dmg = os.path.abspath(os.path.join(OUTPUT_DIR, 'dmg', f'{self.base_num}_dmg.png'))
+
+    def check_base_num(self):
+        """
+        Checks that the number sequence matches for both the pre and post images. This should ensure the pre and post
+        images cover the same extent.
+        :return: True if numbers match
+        """
+        pre_base = ''.join([digit for digit in self.pre if digit.isdigit()])
+        post_base = ''.join([digit for digit in self.post if digit.isdigit()])
+        if pre_base == post_base:
+            return pre_base
+
+    def infer(self):
+        """
+        Passes object to inference.
+        :return: True if successful
+        """
+
+        self.opts = inference.Options(self.pre, self.post, self.output_loc, self.output_dmg),
+
+        try:
+            # TODO: Not sure why opts seems to be a list.
+            inference.main(self.opts[0])
+        except Exception as e:
+            print(f'File: {self.pre}. Error: {e}')
+            return False
+
+        return True
+
+    def georef(self):
+        pass
 
 
 def main():
 
-    pre_files = get_files(PRE_DIR)
-    post_files = get_files(POST_DIR)
-    file_valid_check(pre_files, post_files)
+    pre_files = sorted(get_files(PRE_DIR))
+    post_files = sorted(get_files(POST_DIR))
+    string_len_check(pre_files, post_files)
+    file_objs = []
+    for pre, post in zip(pre_files, post_files):
+        file_objs.append(Files(pre, post))
 
+    for obj in file_objs:
+        obj.infer()
+
+def make_output_structure(path):
+    pass
 
 
 def get_files(where, which='*.png'):
+    """
+    Gathers list of files from path
+    :param where: Path to search
+    :param which: string to match (default: *.png)
+    :return: List of files in path matching rule
+    """
 
     rule = re.compile(fnmatch.translate(which), re.IGNORECASE)
     return [name for name in os.listdir(where) if rule.match(name)]
 
 
-def file_valid_check(pre, post):
+def string_len_check(pre, post):
 
     if len(pre) != len(post):
+        # TODO: Add some helpful info on why this failed
         return False
-    else:
-        return True
 
+    return True
 
 
 if __name__ == '__main__':
--- test/test_inference.py
+++ test/test_inference.py
@@ -1,34 +1,59 @@
 import unittest
+import os
 
 import inference
 import handler
 
 
-class TestHandler(unittest.TestCase):
+class TestGetFiles(unittest.TestCase):
 
     def test_get_files(self):
-        self.result = handler.get_files('input/post')
+        self.path = 'input/post'
+        self.result = handler.get_files(self.path)
         self.list_pre = ['hurricane-matthew_00000351_post_disaster.png']
         self.assertEqual(self.list_pre, self.result)
 
     def test_get_files_mixed_case(self):
-        self.result = handler.get_files('input/pre')
+        self.path = 'input/pre'
+        self.result = handler.get_files(self.path)
         self.list_pre = ['hurricane-matthew_00000351_pre_disaster.PNG']
         self.assertEqual(self.list_pre, self.result)
+
+
+class TestFileValidCheck(unittest.TestCase):
+
+    def setUp(self):
+        # TODO: Hold strings here to be passed to lists
+        pass
 
     def test_string_equal_length(self):
         self.st1 = ['hurricane-matthew_00000351_pre_disaster.png']
         self.st2 = ['hurricane-matthew_00000351_pre_disaster.PNG']
-        self.assertTrue(handler.file_valid_check(self.st1, self.st2))
+        self.assertTrue(handler.string_len_check(self.st1, self.st2))
 
     def test_string_unequal_length(self):
         self.st1 = ['hurricane-matthew_00000351_pre_disaster.PNG',
-               'hurricane-matthew_00000351_post_disaster.png']
+                    'hurricane-matthew_00000351_post_disaster.png']
         self.st2 = ['hurricane-matthew_00000351_pre_disaster.PNG']
-        self.assertFalse(handler.file_valid_check(self.st1, self.st2))
+        self.assertFalse(handler.string_len_check(self.st1, self.st2))
 
-    def test_name_mismatch(self):
+    def test_no_files(self):
         pass
+
+    def test_do_inference(self):
+        pass
+
+
+class TestFilesClass(unittest.TestCase):
+
+    def setUp(self):
+        self.test_object = handler.Files('hurricane-matthew_00000351_pre_disaster.PNG', 'hurricane-matthew_00000351_post_disaster.PNG')
+
+    def test_infer(self):
+        self.assertTrue(self.test_object)
+
+    def test_base_num_match(self):
+        self.assertTrue(self.test_object.check_base_num())
 
 
 class TestInference(unittest.TestCase):
