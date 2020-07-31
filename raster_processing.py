5e8b86ce632d963461e710a07b53c20b70564236
EPound <epound@gmail.com>
2020-07-31 09:10:19 -0400

    create_mosaic

--- raster_processing.py
+++ raster_processing.py
@@ -5,25 +5,23 @@ import subprocess
 import os
 
 
-def create_mosaic(path, in_files, out_file='output/staging/mosaic.tif'):
+def create_mosaic(in_files, out_file='output/staging/mosaic.tif'):
 
-    src_files = []
-
-    for file in in_files:
-        src = rasterio.open(file)
-        src_files.append(src)
+    src_files = [rasterio.open(file) for file in in_files]
 
     mosaic, out_trans = rasterio.merge.merge(src_files)
 
     rasterio.plot.show(mosaic, cmap='terrain')
 
-    out_meta = src.meta.copy()
+    # TODO: Add in metadata: https://automating-gis-processes.github.io/CSC18/lessons/L6/raster-mosaic.html
+    out_meta = src_files[0].meta.copy()
 
     out_meta.update({"driver": "GTiff",
                      "height": mosaic.shape[1],
                      "width": mosaic.shape[2],
                      "transform": out_trans,
-                     "crs": "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs "
+                     # TODO: Update our crs
+                     "crs": "+proj=utm +zone=15 +ellps=GRS80 +units=m +no_defs "
                      }
                     )
 
--- tests/raster_processing/test_raster_processing.py
+++ tests/raster_processing/test_raster_processing.py
@@ -1,17 +1,30 @@
 import unittest
 import os
+import rasterio
 
 import raster_processing
+import handler
 
 
 class TestCreateMosaic(unittest.TestCase):
 
+    def setUp(self):
+        self.files = handler.get_files('../data/input/pre')
+        self.out_file = os.path.abspath('../data/output/test_mosaic.tif')
+        try:
+            os.remove(self.out_file)
+        except:
+            pass
+
+        self.result = raster_processing.create_mosaic(self.files, out_file=self.out_file)
+
     def test_output_exists(self):
-        # TODO: Remove this file prior to calling the function
-        self.path = '../data/input/staging'
-        self.files = [
-            f'{os.path.abspath("input/post/hurricane-matthew_00000351_post_disaster.png")}',
-            #f'{os.path.abspath("input/pre/hurricane-matthew_00000351_pre_disaster.PNG")}'
-                      ]
-        result = raster_processing.create_mosaic(self.path, self.files)
-        self.assertTrue(os.path.isfile(result))
+        self.assertTrue(os.path.isfile(self.result))
+
+    def test_correct_resolution(self):
+        self.src = rasterio.open(self.result)
+        self.assertEqual((0.6, 0.6), self.src.res)
+
+    def test_correct_extent(self):
+        self.src = rasterio.open(self.result)
+        self.assertEqual((355207.8, 4110082.2), self.src.transform * (0, 0))
