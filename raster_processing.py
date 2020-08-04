18f542da0ac87ac2ea159a81d53f18afd06e5422
EPound <epound@gmail.com>
2020-08-04 14:12:02 -0400

    change default chip size

--- raster_processing.py
+++ raster_processing.py
@@ -91,18 +91,18 @@ def create_chips(in_raster, out_dir):
 
     output_filename = 'tile_{}-{}.tif'
 
-    def get_tiles(ds, width=256, height=256):
+    def get_tiles(ds, width=1024, height=1024):
         nols, nrows = ds.meta['width'], ds.meta['height']
         offsets = product(range(0, nols, width), range(0, nrows, height))
         big_window = windows.Window(col_off=0, row_off=0, width=nols, height=nrows)
-        for col_off, row_off in  offsets:
+        for col_off, row_off in offsets:
             window = windows.Window(col_off=col_off, row_off=row_off, width=width, height=height).intersection(big_window)
             transform = windows.transform(window, ds.transform)
             yield window, transform
 
 
     with rasterio.open(in_raster) as inds:
-        tile_width, tile_height = 256, 256
+        tile_width, tile_height = 1024, 1024
 
         meta = inds.meta.copy()
         print(meta)
