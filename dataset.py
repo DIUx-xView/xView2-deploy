abb3f6b0bd834f7c1e82d2a19adbe00f40ecc948
Jared Dunnmon <jdunnmon@gmail.com>
2020-08-23 04:02:32 -0400

    removed extra dataset

--- dataset.py
+++ dataset.py
@@ -40,39 +40,3 @@ class XViewDataset(Dataset):
         out_dict['is_vis'] = fl.opts.is_vis
 
         return out_dict
-
-class XViewDataset2(Dataset):
-    "Dataset for xView"
-
-    def __init__(self, pre_chips, post_chips, args, config, transform=None):
-        """
-        :param pre_chips: List of pre-damage chip filenames
-        :param post_chips: List of post_damage chip filenames
-        :param transform: PyTorch transforms to be used on each example
-        """
-        self.pre_directory = args.pre_directory,
-        self.post_directory = args.post_directory,
-        self.pre_chips = pre_chips
-        self.post_chips = post_chips
-        self.transform = transform
-        self.config = config
-
-        assert len(pre_chips) == len(post_chips)
-
-    def __len__(self):
-        return(len(self.pre_chips))
-
-    def __getitem__(self, idx):
-        pre=os.path.join(self.pre_directory, self.pre_chips[idx])
-        post=os.path.join(self.post_directory,self.post_chips[idx])
-        if self.config.DATASET.IS_TIFF:
-            pre_image = tifffile.imread(args.in_pre_path)
-            post_image = tifffile.imread(args.in_post_path)
-        else:
-            pre_image = imread(args.in_pre_path)
-            post_image = imread(args.in_post_path)
-
-        pre = self.transform(pre)
-        post = self.transform(post)
-
-        return pre , post
