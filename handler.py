cf34d9f18fbf2616694c41fa66083be1b27ca72a
EPound <epound@gmail.com>
2020-09-06 06:34:02 -0400

    Fix freeze support import.

--- handler.py
+++ handler.py
@@ -550,6 +550,6 @@ def main():
 
 if __name__ == '__main__':
     if os.name == 'nt':
-        from multiprocessing import freeze_support()
+        from multiprocessing import freeze_support
         freeze_support()
     main()
