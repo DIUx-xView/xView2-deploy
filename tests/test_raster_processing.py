59a6d01144f6b05aed63b7e8019e2e1cc6faeb4a
EPound <epound@gmail.com>
2020-09-24 07:40:38 +0300

    Add no_crs data for tests.

--- tests/test_raster_processing.py
+++ tests/test_raster_processing.py
@@ -1,57 +1,63 @@
-from unittest import TestCase
-import os
 import rasterio
 from pathlib import Path
 
 import raster_processing
-import handler
 
 
-class TestReproject(TestCase):
-
-    def test_reproject(self):
-        self.in_file = Path('data/input/pre/tile_337-10160.tif')
-        self.dest_file = Path('data/output/resample.tif')
-        self.result = raster_processing.reproject(self.in_file, self.dest_file, None)
-        self.test = rasterio.open(self.result).crs
-        self.assertEqual('EPSG:4326', self.test)
+def test_get_intersect():
+    test = raster_processing.get_intersect(
+        Path('data/output/mosaics/pre.tif'),
+        Path('data/output/mosaics/post.tif')
+    )
+    assert (-94.49960529516346, 37.06631597942802, -94.48623559881267, 37.07511383680346) == test
 
 
-class TestCreateMosaic(TestCase):
+def test_reproject_crs_set(tmp_path):
+    # Test file with input having CRS set
 
-    def setUp(self):
-        self.files = handler.get_files(Path('data/input/pre'))
-        self.out_file = Path('data/output/test_mosaic.tif')
-        try:
-            os.remove(self.out_file)
-        except:
-            pass
-
-        self.result = raster_processing.create_mosaic(self.files, out_file=self.out_file)
-
-    def test_output_exists(self):
-        self.assertTrue(os.path.isfile(self.result))
-
-    def test_correct_resolution(self):
-        self.src = rasterio.open(self.result)
-        self.assertEqual((0.6, 0.6), self.src.res)
-
-    def test_correct_extent(self):
-        self.src = rasterio.open(self.result)
-        self.assertEqual((366642.60000000003, 4104511.1999999997), self.src.transform * (0, 0))
+    in_file = Path('data/input/pre/tile_337-10160.tif')
+    dest_file = tmp_path / 'resample.tif'
+    result = raster_processing.reproject(in_file, dest_file, None, 'EPSG:4326')
+    with rasterio.open(result) as src:
+        test = src.crs
+    assert test == 'EPSG:4326'
 
 
-class TestGetIntersect(TestCase):
+def test_reproject_no_crs_set(tmp_path):
+    # Test file with input file having no CRS set
 
-    def test_intersect_extent(self):
-        self.test = raster_processing.get_intersect(
-            Path('data/input/pre/tile_337-10160.tif'),
-            Path('data/input/post/tile_31500-6161.tif')
-        )
-        self.assertEqual((364950.125, 4102656.6, 366951.60000000003, 4105049.875),
-                         self.test)
-
-
+    in_file = Path('data/misc/no_crs/may24C350000e4102500n.jpg')
+    dest_file = tmp_path / 'resample.tif'
+    result = raster_processing.reproject(in_file, dest_file, 'EPSG:26915', 'EPSG:4326')
+    with rasterio.open(result) as src:
+        test = src.crs
+    assert test == 'EPSG:4326'
+#
+#
+# class TestCreateMosaic(TestCase):
+#
+#     def setUp(self):
+#         self.files = handler.get_files(Path('data/input/pre'))
+#         self.out_file = Path('data/output/test_mosaic.tif')
+#         try:
+#             os.remove(self.out_file)
+#         except:
+#             pass
+#
+#         self.result = raster_processing.create_mosaic(self.files, out_file=self.out_file)
+#
+#     def test_output_exists(self):
+#         self.assertTrue(os.path.isfile(self.result))
+#
+#     def test_correct_resolution(self):
+#         self.src = rasterio.open(self.result)
+#         self.assertEqual((0.6, 0.6), self.src.res)
+#
+#     def test_correct_extent(self):
+#         self.src = rasterio.open(self.result)
+#         self.assertEqual((366642.60000000003, 4104511.1999999997), self.src.transform * (0, 0))
+#
+#
 # class TestCreateChips(unittest.TestCase):
 #
 #     def setUp(self):
@@ -68,8 +74,8 @@ class TestGetIntersect(TestCase):
 #         self.test = 'Window(col_off=211, row_off=10114, width=10853, height=11148)'
 #         self.result = raster_processing.get_intersect_win(self.mosaic, self.intersect)
 #         self.assertEqual(self.test, self.result)
-
-
-class TestCheckDims(TestCase):
-
-    pass
+#
+#
+# class TestCheckDims(TestCase):
+#
+#     pass
