e9a54169857398e76ffb7a89296ec40b94d5e940
EPound <epound@gmail.com>
2021-02-23 08:28:11 +0300

    Fix mosaic.

--- handler.py
+++ handler.py
@@ -199,7 +199,10 @@ def postprocess_and_write(result_dict):
         mask_map_img[cls == 2] = (229, 255, 50)
         mask_map_img[cls == 3] = (255, 159, 0)
         mask_map_img[cls == 4] = (255, 0, 0)
-        compare_img = np.concatenate((sample_result_dict['in_pre_path'], mask_map_img), axis=1)
+
+        with rasterio.open(sample_result_dict['in_pre_path']) as pre:
+            pre_image = pre.read()
+        compare_img = np.concatenate((pre_image, mask_map_img), axis=1)
 
         # debug
         # cv2.imwrite('test_map.png',mask_map_img,[cv2.IMWRITE_PNG_COMPRESSION, 9])
