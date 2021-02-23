36b73ebf094b7eb15bfdc6e67acb4d5b74c52a67
EPound <epound@gmail.com>
2021-02-23 10:29:21 +0300

    Fix mosaic. Testing required.

--- handler.py
+++ handler.py
@@ -196,8 +196,8 @@ def postprocess_and_write(result_dict):
         compare_img = np.concatenate((pre_image, mask_map_img), axis=1)
 
         # debug
-        np.save('home/ubuntu/data/mask_arr.npy', mask_map_img)
-        np.save('home/ubuntu/data/img_arr.npy', pre_image)
+        np.save('mask_arr.npy', mask_map_img)
+        np.save('img_arr.npy', pre_image)
 
         # debug
         # cv2.imwrite('test_map.png',mask_map_img,[cv2.IMWRITE_PNG_COMPRESSION, 9])
